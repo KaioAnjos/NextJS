6a244517477dd44ea7a8b6ea1ee0f64cf3a60478
KaioAnjos <kaio11anjos@gmail.com>
2022-07-11 10:18:38 -0300

    Update TacladoPress.jsx
    Teste de input

--- fundamentos/src/components/TacladoPress.jsx
+++ fundamentos/src/components/TacladoPress.jsx
@@ -6,12 +6,20 @@ export default function Teclado_Press(){
     const w = document.querySelector("in");
     String.toString(document.getElementById("letra").innerText = "Número: " + x + " = Caractere: " + y);
     if(x == "112") {String.toString(document.getElementById("F1").innerText = "F1 precionado");} 
+    if(x == "13"){
+        var g = "<input type='text' id='input' size='40'/>";
+        document.getElementById("input").innerHTML = g;
+    }
+    else{
+        document.getElementById("input").innerText = "Aberte Enter"
+    }
 }
     return(
         <div>
         <p>Tecla precionado</p>
         <input id="in" type="text" size="40" onKeyDown={() => dig(event)}/>
         <p id="letra"></p>
+        <div id="input"></div>
         <p id="F1"></p>
         </div>
     );
