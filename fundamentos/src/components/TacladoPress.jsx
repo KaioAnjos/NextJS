74013547c22f42ca336736035fcc1ea9f4b9af03
KaioAnjos <kaio11anjos@gmail.com>
2022-07-11 09:47:03 -0300

    Update TacladoPress.jsx
    Teste do Leitor de Teclado

--- fundamentos/src/components/TacladoPress.jsx
+++ fundamentos/src/components/TacladoPress.jsx
@@ -3,16 +3,16 @@ export default function Teclado_Press(){
     function dig(event){
     var x = event.keyCode;
     var y = String.fromCharCode(x);
-    var b;
     const w = document.querySelector("in");
     String.toString(document.getElementById("letra").innerText = "Número: " + x + " = Caractere: " + y);
-    if(x == "112") {b.toString ="TECLA f1"; return(b)} 
+    if(x == "112") {String.toString(document.getElementById("F1").innerText = "F1 precionado");} 
 }
     return(
         <div>
         <p>Tecla precionado</p>
         <input id="in" type="text" size="40" onKeyDown={() => dig(event)}/>
         <p id="letra"></p>
+        <p id="F1"></p>
         </div>
     );
 }
