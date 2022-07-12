b60aa740f78bbaaff94d85b59b813ff9b78e7d04
KaioAnjos <kaio11anjos@gmail.com>
2022-07-12 16:08:00 -0300

    Update TacladoPress.jsx
    adição da detecção das teclas x e c para inserir o código de barra

--- fundamentos/src/components/TacladoPress.jsx
+++ fundamentos/src/components/TacladoPress.jsx
@@ -1,39 +1,45 @@
 import React from "react";
-import Jquery from "jquery";
-export default function Teclado_Press(){
-    function dig(event){
-    var x = event.keyCode;
-    var y = String.fromCharCode(x);
-    String.toString(document.getElementById("letra").innerText = "Número: " + x + " = Caractere: " + y);
-    if(x == "112") {String.toString(document.getElementById("F1").innerText = "F1 precionado");} 
-    if(x == "13"){
-        var g = "<input type='text' id='teste' size='40'/>";
-        document.getElementById("input").innerHTML = g;
-        document.getElementById("teste").focus();
+export default function Teclado_Press() {
+    function dig(event) {
+        var x = event.keyCode;
+        var y = String.fromCharCode(x);
+        String.toString(document.getElementById("letra").innerText = "Número: " + x + " = Caractere: " + y);
+        if (x == "112") { String.toString(document.getElementById("F1").innerText = "F1 precionado"); }
+        if (x == "13") {
+            var g = "<input type='text' id='teste' size='40'/>";
+            document.getElementById("input").innerHTML = g;
+            document.getElementById("teste").focus();
+        }
+        else {
+            var g = "<input type='text' id='teste' size='40' disabled/>"
+            document.getElementById("input").innerHTML = g;
+        }
+        if (x == "67") {
+            document.getElementById("inp").focus();
+            document.getElementById("in").value = "";
+            setTimeout(() => { document.getElementById("inp").value = "" }, 100);
+        }
+        if (x == "88") {
+            var captu = "";
+            function cap() {
+                var c = captu = parseInt(document.getElementById('inp').value);
+                document.getElementById("in").focus();
+                setTimeout(() => { document.getElementById("in").value = "" }, 100);
+                return c;
+            }
+            alert(cap())
+        }
     }
-    else{
-        var g = "<input type='text' id='teste' size='40' disabled/>"
-        document.getElementById("input").innerHTML = g;
-    }
-   
-    if(x == "88"){
-        var captu = "";
-        function cap(){
-            captu = document.getElementById('in').value;
-        return captu;
-    }
-    alert(cap());
-    }
-}
-       setTimeout(() => {document.getElementById("in").focus();}, 500); 
-        return(
-       <div>
-        <p>Tecla precionado</p>
-        <input id="in" type="text" size="40" onKeyDown={() => dig(event)} name="in" />
-        <p id="letra"></p>
-        <p id="input"></p>
-        <p id="F1"></p>
-        <p id="Calcular"></p>
+    setTimeout(() => { document.getElementById("in").focus(); }, 500);
+    return (
+        <div>
+            <p>Tecla precionado</p>
+            <input id="inp" type="text" size="1" onKeyDown={() => dig(event)} />
+            <input id='in' type="text" size="40" onKeyDown={() => dig(event)} />
+            <p id="letra"></p>
+            <p id="input"></p>
+            <p id="F1"></p>
+            <p id="Calcular"></p>
         </div>
-        )
+    )
 }
