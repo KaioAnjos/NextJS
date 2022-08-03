b7b8319c03a9e822364ab04e92fd68e9a5ee9479
KaioAnjos <kaio11anjos@gmail.com>
2022-08-03 11:39:09 -0300

    teste

--- next-crud/backend/config.ts
+++ next-crud/backend/config.ts
@@ -1,7 +1,11 @@
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 import 'firebase/firestore'
+import 'firebase/auth';
+import "firebase/database";
+import "firebase/storage";
 
-if(!firebase.getApps.length){
+if(!firebase.initializeApp.length){
+    
     firebase.initializeApp({
         apiKey:  process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
