9364d9b31b6f82ab17c8e562dcfc16a9744c194c
KaioAnjos <kaio11anjos@gmail.com>
2022-08-04 11:28:08 -0300

    Erro conexão firebase

--- next-crud/backend/config.ts
+++ next-crud/backend/config.ts
@@ -1,16 +1,14 @@
-import firebase from "firebase/app";
-import 'firebase/firestore'
-import 'firebase/auth';
-import "firebase/database";
-import "firebase/storage";
+import firebase from "firebase";
+import "firebase/app";
+import "firebase/firestore";
+import "firebase/app";
 
-if(!firebase.initializeApp.length){
-    
-    firebase.initializeApp({
-        apiKey:  process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    })
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  });
 }
 
-export default firebase
+export default firebase;
--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,52 +1,63 @@
-import { useState } from "react";
+import { async } from "@firebase/util";
+import { useEffect, useState } from "react";
+import ColecaoCliente from "../../backend/DB/ColecaoCliente";
 import Cliente from "../../core/Cliente";
+import ClienteRepostorio from "../../core/ClienteRepositorio";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 export default function Home() {
+  const repo: ClienteRepostorio = new ColecaoCliente();
 
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
-  const clientes =[
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 45, '2'),
-    new Cliente('Carlos', 23, '3'),
-    new Cliente('Pedro', 54, '4'),
-    ]
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio());
+  const [clientes, setClientes] = useState<Cliente[]>([]);
+  useEffect(obterTodos, []);
+   function obterTodos(){
+    repo.obterTodos().then(() => {
+      setClientes(clientes)
+      setVisivel("tabela");
+    });
+   }
 
-    function clienteSelecionado(cliente: Cliente){
-      setCliente(cliente)
-      setVisivel('form')
-    }
-    function clienteExcluido(cliente: Cliente){
-      console.log(`Excluir.... ${cliente.Tnome}`)
-    }
+  function clienteSelecionado(cliente: Cliente) {
+    setCliente(cliente);
+    setVisivel("form");
+  }
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir.... ${cliente.Tnome}`);
+  }
 
-    function salvarCliente(cliente:Cliente){
-      setVisivel('tabela')
-    }
+  async function salvarCliente(cliente: Cliente) {
+    await repo.salvar(cliente)
+    obterTodos();
+  }
 
-    function novoCliente(){
-      setCliente(Cliente.vazio())
-      setVisivel('form')
-    }
-    
+  function novoCliente() {
+    setCliente(Cliente.vazio());
+    setVisivel("form");
+  }
 
   return (
-  
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
       <Layout titulo="Cadastro Simples">
-        {visivel === 'tabela' ?(
-           <>
-           <Botao   className="mb-4" cor="green" onClick={novoCliente}>Novo Cliente</Botao>
-          <Tabela clientes={clientes} 
-           clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}/>
-           </>
-        ):(
-          <Formulario cliente={cliente}
-          clienteMudou={salvarCliente}
-          cancelado={() => setVisivel('tabela')}
+        {visivel === "tabela" ? (
+          <>
+            <Botao className="mb-4" cor="green" onClick={novoCliente}>
+              Novo Cliente
+            </Botao>
+            <Tabela
+              clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            />
+          </>
+        ) : (
+          <Formulario
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={() => setVisivel("tabela")}
           />
         )}
       </Layout>
